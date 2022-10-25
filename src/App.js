9a71575314a9dae9eb552e660daced165167c425
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-10-26 00:12:45 +0100

    added the share button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Links from "./Links.js";
 function App() {
   return (
     <div className="App">
-      <img id="profile__img" src={require("./marv.jpg")}></img>
+      <div className="header_div">
+        <img id="profile__img" src={require("./profile__img.png")}></img>
+        <div className="overlay">
+          <img id="ig-icon" src={require("./Icon.png")} />
+        </div>
+      </div>
+      <img id="share-icon" src={require("./share-icon.png")} />
+      <div id="mobile_menu">
+        <img id="mobile-btn" src={require("./mobile-btn.png")} />
+      </div>
+
       <p id="twitter">@BeLikeMarv</p>
       <p id="slack">Marvellous Adesanya</p>
       <Links />
-      <img></img>
-      <img></img>
     </div>
   );
 }
