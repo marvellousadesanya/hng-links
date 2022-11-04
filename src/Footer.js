a9983856f99baa5ddc068f460510d2050b70ba38
Marvellous Adesanya <marvellousniyi123@gmail.com>
2022-11-04 14:48:50 +0100

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function Footer() {
+  return (
+    <>
+      <img src={require("./Icon.png")}></img>
+      <p>HNG Zuri Internship</p>
+      <img src={require("./I4G.png")}></img>
+    </>
+  );
+}
